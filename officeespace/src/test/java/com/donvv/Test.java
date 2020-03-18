4936dbc7a6362aa798429b42904d1ba694be3810
xxl <a540153772@163.com>
2020-03-18 09:27:20 +0800

    1234

---
+++ officeespace/src/test/java/com/donvv/Test.java
@@ -0,0 +1,14 @@
+package com.donvv;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class Test {
+
+    @org.junit.Test
+    public void test1(){
+        System.out.println("123");
+    }
+}
